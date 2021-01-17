90e6ddf7276071066bf15a64e5e1ef94df2905c1
tugbaca <tugbacan57@outlook.com>
2021-01-17 18:47:11 +0300

    added abstrat detector

---
+++ Detector/Detector.py
@@ -0,0 +1,26 @@
+from Detector.AbstractDetector import AbstractDetector
+import cv2
+from Detector.MatchFeature.Match import Match_Features
+from DrawFunctions.Rectangle import DrawRectangle
+from DrawFunctions.Line import DrawLine
+
+class AbsDetector:
+    key_points = None
+    descriptors = None
+    color = None
+    image = None
+    distance = None
+
+    def __init__(self, image, key_points, descriptors, distance, color):
+        self.image = image
+        self.key_points = key_points
+        self.descriptors = descriptors
+        self.distance = distance
+        self.color = color
+        self.detector()
+
+    def detector(self):
+        match = Match_Features(self.key_points,self.descriptors,self.distance)
+        draw = DrawRectangle(self.image, match.gPoint1, match.gPoint2, self.color, match.cRectangle)
+        #draw = DrawLine(self.image,keypoints1 = points1, keypoints2 = points2,color=self.color)
+        self.image = draw.image
