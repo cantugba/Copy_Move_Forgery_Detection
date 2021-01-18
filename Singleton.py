0d31e4697bec9c9a962c550ee27a94a4e412a2f0
tugbaca <tugbacan57@outlook.com>
2021-01-18 16:23:13 +0300

    update

--- Singleton.py
+++ Singleton.py
@@ -3,7 +3,6 @@ class SingletonMeta(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
